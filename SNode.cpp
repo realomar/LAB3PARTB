a008fcc077bfb0a6152802ff6fb4c54d901b333d
Tamimoz <tamimoz@udel.edu>
2018-10-06 23:28:03 -0400

    comments part B

--- SNode.cpp
+++ SNode.cpp
@@ -9,7 +9,7 @@
 #include <stdlib.h>
 using namespace std;
 
-SNode::SNode(){ // this constructor reads values from input
+SNode::SNode(){ // this constructor reads values from the user inputs in the console ad sets up the node and its data by using the user's input in console.
 	int rating;
 	string comments;
 	cout << "Enter a numerical rating: ";
@@ -21,13 +21,13 @@ SNode::SNode(){ // this constructor reads values from input
 	next = NULL;
 }
 
-SNode::SNode(int r, string c){ // this constructor reads values from parameter
+SNode::SNode(int r, string c){ // this constructor reads values from parameter to set up the node and it's accompanying data
 	this->rating = r;
 	this->comments = c;
 	next = NULL;
 }
 
-void SNode::printNode(){ // prints node information
+void SNode::printNode(){ // prints node information: Rating and comments, in one line.
 	cout << "Rating: " << rating << ", ";
 	cout << "Comments: " << comments << endl;
 }
--- SNode.hpp
+++ SNode.hpp
@@ -11,15 +11,24 @@
 using namespace std;
 
 class SNode{
-	friend class SLL;
-	int rating;
-	string comments;
+	friend class SLL; //SLL can access the functions and variables inside SNode
+	int rating; //Two types of data, one is rating
+	string comments; //The other is comments
 	SNode *next;
 
 public:
+	//Constructors:
+
 	SNode();
 	SNode(int r, string c);
+
+	//Destructor:
+
 	~SNode();
+
+	//Methods:
+
 	void printNode();
+
 };
 #endif /* SNODE_HPP_ */

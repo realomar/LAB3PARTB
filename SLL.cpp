4db0abe7d34136cc03aa7f21166f132c3283efcc
Tamimoz <tamimoz@udel.edu>
2018-10-06 23:35:59 -0400

    FINAL COMMIT

--- SLL.cpp
+++ SLL.cpp
@@ -26,7 +26,7 @@ SLL::~SLL(){
 	}
 }
 
-void SLL::printSLL(){
+void SLL::printSLL(){ //Prints out the linked list.
 	SNode *i = first;
 	while(i != NULL){
 		i->printNode();
@@ -36,6 +36,13 @@ void SLL::printSLL(){
 }
 
 void SLL::insertInOrder(int r, string c){
+	//This method creates an ordered linked list (ordered by the rating). If the
+	//linked list is empty, it calls addFirst with r and c. If the rating is less
+	//than the first node’s rating in the list, it calls addAtFirst. If the
+	//rating is greater than the last node’s rating, it calls the push method.
+	//Otherwise it creates a new node using r and c, and inserts that node in
+	//order into the linked list.
+
 	if(first == NULL){
 		addFirst(r,c);
 	}
@@ -63,27 +70,27 @@ void SLL::insertInOrder(int r, string c){
 	}
 }
 
-void SLL::push(int r, string c){
+void SLL::push(int r, string c){ //pushes a new node (with rating r and c comments) onto the end of the linked list
 	SNode *i = new SNode(r,c);
 	last->next = i;
 	last = i;
 	size++;
 }
 
-void SLL::addAtFront(int r, string c){
+void SLL::addAtFront(int r, string c){ // adds a new node (made from r and c) to the beginning of the  list (remember to reset the first pointer) – I called this from inserInOrder(int r, string c) function
 	SNode *i = new SNode(r,c);
 	i->next = first;
 	first = i;
 	size++;
 }
 
-void SLL::addFirst(int r, string c){
+void SLL::addFirst(int r, string c){ //adds the very first node (made from r and c) to an empty list
 	SNode *i = new SNode(r,c);
 	first = last = i;
 	size = 1;
 }
 
-int SLL::pop(){
+int SLL::pop(){ //removes the last node from the linked list, and returns the rating from the node removed
 	if (size > 0) {
 	int returnVal = last->rating;
 
@@ -112,7 +119,7 @@ int SLL::pop(){
 	}
 }
 
-double SLL::getAve(){
+double SLL::getAve(){ //calculates the average rating (by traversing the list) and then returns the average rating.
 	SNode *i = first;
 	double total = 0;
 	while(i->next != NULL){
